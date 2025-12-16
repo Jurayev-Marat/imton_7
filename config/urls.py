be557a2626aed6c5b9289cfa3f5770a9b5c17408
Jurayev.Marat <jurayevmarat23@gmail.com>
2025-12-16 14:34:28 +0500

    loyani oz qizmi qoldi

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,15 @@
-# config/urls.py (asosiy urls.py)
+# config/urls.py (ASOSIY urls.py)
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # Django admin paneli
     path('', include('configapp.urls')),  # Sizning ilovangiz
 ]
+
+# ======= BU QATORLARNI QO'SHING (MAJBOURIY) =======
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
